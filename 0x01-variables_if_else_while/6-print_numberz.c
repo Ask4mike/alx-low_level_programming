60338028e4ac73ee20f82b9c2c045040a3f3646c
Ask4mike <mikaelakpang@gmail.com>
2022-09-12 13:54:18 -0700

    Added the c program to display another set of numbers.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numbz;
+
+	for (numbz = '0'; numbz <= '9'; numbz++)
+	{
+	putchar(numbz);
+	}
+	putchar('\n');
+	return (0);
+}
