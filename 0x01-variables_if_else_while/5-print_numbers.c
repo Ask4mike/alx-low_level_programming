6e0752b39e24780c63985ac6c7bb4f0f6a9f2890
Ask4mike <mikaelakpang@gmail.com>
2022-09-12 13:04:25 -0700

    Added the c program to display a list of numbers.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int numbs;
+
+	for (numbs = 0; numbs <= 9; numbs++)
+	{
+	printf("%d", numbs);
+	}
+	printf("\n");
+	return (0);
+}
