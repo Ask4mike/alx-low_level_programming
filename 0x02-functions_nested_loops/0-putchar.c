007d230d3183298af32e3815b26782d612ff02ea
Ask4mike <mikaelakpang@gmail.com>
2022-09-15 11:59:29 -0700

    Added the c program to print a string.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ * Description - To print a string followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
