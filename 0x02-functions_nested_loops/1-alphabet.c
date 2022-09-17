b254eab77a6d254ac033ae611ea2763089943c96
Ask4mike <mikaelakpang@gmail.com>
2022-09-17 10:06:28 -0700

    added a c function to print the letters of the alphabet.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code 
+ * 
+ * Description - A program used to call functions 
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
