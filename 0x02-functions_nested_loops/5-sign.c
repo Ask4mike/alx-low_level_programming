9b85357acb3f6422e74036ea69489077cddbca0b
Ask4mike <mikaelakpang@gmail.com>
2022-09-17 18:47:11 -0700

    Added the c function that prints the sign of a certain number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * Description - Prints a sign along with a certain number
+ * if all conditions are true.
+ * @n: Int
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
