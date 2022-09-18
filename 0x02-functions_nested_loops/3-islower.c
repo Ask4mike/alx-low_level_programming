241b93ef5577d1fd3e33ac7e811da7b9413af2e1
Ask4mike <mikaelakpang@gmail.com>
2022-09-17 17:12:04 -0700

    added the c function that returns a vlaue after execution.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _islower - Entry point
+ * @c : varable
+ * description : checks if it is caselower
+ * Return: int
+ */
+
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
