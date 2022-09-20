2152609a3a57c1fd486f13e8dfacdf8d95b46b01
Ask4mike <mikaelakpang@gmail.com>
2022-09-20 11:53:29 -0700

    added the c function to count the letters in a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen - Entry point
+ *@s: char - pointer to address of the string.
+ *Description - The function that prints the length of a string.
+ *Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int count, incre;
+
+	incre = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+		incre++;
+	
+	return (incre);
+
+}
