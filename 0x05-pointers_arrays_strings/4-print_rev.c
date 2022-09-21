a6c9c58eadfa876e8a9746dbafed5102cc065e2a
Ask4mike <mikaelakpang@gmail.com>
2022-09-20 18:57:08 -0700

    added the c program to print a string in a reversed format.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * Description - A function that prints a string, in reverse.
+ * @str: A pointer of type char
+ */
+
+void print_rev(char *s)
+{
+	int len = 0, clue = 0;
+
+	len = _strlen(s);
+
+	for (clue = len - 1; clue >= 0; clue--)
+	_putchar(s[clue]);
+	
+	_putchar('\n');
+}
+
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns length
+ */
+
+int _strlen(char *s)
+{
+	int count, incre;
+
+	incre = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	incre++;
+
+	return (incre);
+}
